944f75f1164a3931e8c97cf209ef35835f1a7bd2
Cele <njceles@gmail.com>
2024-02-12 19:27:25 +0200

    created feature file

---
+++ src/test/java/Features/APIDemos.feature
@@ -0,0 +1,5 @@
+Feature: API Demos
+  Scenario: Verify that API demos landing page is displayed
+    Given The app is launched
+    And User verify that the landing is displayed
+#    And User click App
---
+++ src/test/java/Steps/stepsdef.java
@@ -0,0 +1,17 @@
+package Steps;
+
+import io.cucumber.java.en.*;
+
+public class stepsdef {
+    @Given("The app is launched")
+    public void the_app_is_launched() {
+
+    }
+    @And("User verify that the landing is displayed")
+    public void user_verify_that_the_landing_is_displayed() {
+
+    }
+    @And("User click App")
+    public void userClickApp() {
+    }
+}
