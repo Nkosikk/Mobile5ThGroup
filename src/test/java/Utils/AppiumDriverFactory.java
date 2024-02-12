1817eb1fb322f518e136f48c745e923d7377a7a3
Cele <njceles@gmail.com>
2024-02-12 19:12:05 +0200

    created the appium driver factory

---
+++ src/test/java/Utils/AppiumDriverFactory.java
@@ -0,0 +1,40 @@
+package Utils;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AppiumDriverFactory {
+    public static AppiumDriver driver;
+    public static AppiumDriverFactory instanceOfAppiumDriverFactory;
+
+    public AppiumDriverFactory() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("automationName", "UiAutomator2");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("app", System.getProperty("user.dir") + "/src/man/Apps/ApiDemos-debug.apk");
+        capabilities.setCapability("autoAcceptAlert",true);
+
+        try{
+            driver= new AndroidDriver(new URL("http://192.168.0.186:4723/"),capabilities);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    public static AppiumDriverFactory getInstanceOfAppiumDriverFactory(){
+        if (instanceOfAppiumDriverFactory==null)
+            instanceOfAppiumDriverFactory= new AppiumDriverFactory();
+        return instanceOfAppiumDriverFactory;
+    }
+
+    public AppiumDriver getDriver(){
+        return driver;
+    }
+}
